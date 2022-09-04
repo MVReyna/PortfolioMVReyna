24e37280905eaea8344587f340dda803ec1e94b6
MVReyna <vickyreyna79@gmail.com>
2022-09-03 21:06:05 -0300

    creación componente proyectos

--- portfolioMVReyna/src/app/app.module.ts
+++ portfolioMVReyna/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,

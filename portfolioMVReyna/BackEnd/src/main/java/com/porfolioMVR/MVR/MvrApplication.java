15bd3052a90f683828d67d1a9072adbde706b524
MVReyna <vickyreyna79@gmail.com>
2022-09-08 20:33:18 -0300

    creación carpeta backend
    Se inició el proyecto en java y transfirió a la carpeta backend

---
+++ portfolioMVReyna/BackEnd/src/main/java/com/porfolioMVR/MVR/MvrApplication.java
@@ -0,0 +1,13 @@
+package com.porfolioMVR.MVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvrApplication.class, args);
+	}
+
+}
